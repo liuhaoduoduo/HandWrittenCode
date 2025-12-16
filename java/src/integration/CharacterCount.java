86be118b5c5890b82c7e6cde746686117f0354db
wb_liuhao29 <wb_liuhao29@meituan.com>
2025-12-16 19:28:01 +0800

    整理格式

--- java/src/integration/CharacterCount.java
+++ java/src/integration/CharacterCount.java
@@ -9,22 +9,22 @@ import java.util.Set;
 
 public class CharacterCount {
     public static int countUniqueCharacters(String str) {
-        //如果字符串是null或空字符串，直接返回0
-        if (str == null||str.isEmpty()) {
+        // 如果字符串是null或空字符串，直接返回0
+        if (str == null || str.isEmpty()) {
             return 0;
         }
-        //存放字符的set集合
+        // 存放字符的set集合
         Set<Character> uniqueChars = new HashSet<>();
-        //遍历字符串中的字符
+        // 遍历字符串中的字符
         for (char c : str.toCharArray()) {
             uniqueChars.add(c);
         }
-        //返回数量
+        // 返回数量
         return uniqueChars.size();
     }
 
     public static void main(String[] args) {
-        //待处理字符串
+        // 待处理字符串
         String str = "Long live People's Republic of China";
         int count = countUniqueCharacters(str);
         System.out.println("字符串 \"" + str + "\" 中不同字符的个数为: " + count);
